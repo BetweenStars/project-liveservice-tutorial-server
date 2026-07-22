de0a00efbf6aa0d3ad2175069cb6c45e9fc2e139
BetweenStars <kyubin2001@gmail.com>
2026-07-22 14:12:31 +0900

    db : add nickname field at db

--- obj/Debug/net10.0/GameServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b14e4d1e318ed258b5996459d0e3303bacceea0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64f1be6daeed17dc79119ec44e375b733295e1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
